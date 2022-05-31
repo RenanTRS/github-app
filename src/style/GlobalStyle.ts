98cdff3d8574c611a1fdf457c78487f45fde7ac5
Renan Souza <renantrsouza@gmail.com>
2022-05-31 12:31:14 -0400

    Create GlobalStyle

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:root {
+    font-size: 62.5%;
+}
+
+body{
+    min-height: 100vh;
+    width: 100%;
+}
+`
