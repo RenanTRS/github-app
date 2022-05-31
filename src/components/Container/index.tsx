7eb8b0e09d4815b7e7af2f76838066f33faf9b99
Renan Souza <renantrsouza@gmail.com>
2022-05-31 13:53:51 -0400

    Add style padding-left

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,9 +1,10 @@
 import { Header } from '../Header'
+import { Main } from './style'
 
 export const Container = () => {
   return (
-    <>
+    <Main>
       <Header />
-    </>
+    </Main>
   )
 }
--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components'
 
 export const Input = styled.input`
   flex: 2;
+
+  padding-left: 1rem;
 `
