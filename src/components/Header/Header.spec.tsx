3553b38d025e077ddd791109cfa8285c4743171c
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:07:17 -0400

    Refact test for Header

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react'
-import { ThemeProvider } from 'styled-components'
 import { Header } from '.'
+import { ThemeProvider } from 'styled-components'
 import light from 'style/themes/light'
 
 describe('Header Component', () => {
@@ -12,24 +12,37 @@ describe('Header Component', () => {
       </ThemeProvider>
     )
   }
-  it('should render a input', () => {
-    render(<MockThemeProvider />)
+  describe('Input', () => {
+    it('should render a input', () => {
+      render(<MockThemeProvider />)
 
-    const inputElement = screen.getByRole('textbox')
-    expect(inputElement).toBeInTheDocument()
+      const inputElement = screen.getByRole('textbox')
+      expect(inputElement).toBeInTheDocument()
+    })
+    it('should type into input', () => {
+      render(<MockThemeProvider />)
+
+      const inputElement = screen.getByRole('textbox') as HTMLInputElement
+      fireEvent.change(inputElement, { target: { value: 'something' } })
+      expect(inputElement.value).toBe('something')
+      expect(inputElement.value).not.toBe('SomeThing')
+    })
   })
-  it('should render a button', () => {
-    render(<MockThemeProvider />)
 
-    const buttonElement = screen.getByRole('button')
-    expect(buttonElement).toBeInTheDocument()
-  })
-  it('should call a function', () => {
-    render(<MockThemeProvider />)
+  describe('Button', () => {
+    it('should render a button', () => {
+      render(<MockThemeProvider />)
 
-    const buttonElement = screen.getByRole('button')
-    fireEvent.click(buttonElement)
+      const buttonElement = screen.getByRole('button')
+      expect(buttonElement).toBeInTheDocument()
+    })
+    it('should call a function', () => {
+      render(<MockThemeProvider />)
 
-    expect(callback).toBeCalledTimes(1)
+      const buttonElement = screen.getByRole('button')
+      fireEvent.click(buttonElement)
+
+      expect(callback).toBeCalledTimes(1)
+    })
   })
 })
