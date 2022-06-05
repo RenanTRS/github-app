d55202874e082202d326bd6fb6ca98ef4be516ad
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:08:52 -0400

    Create types

---
+++ src/types/ReposType/index.ts
@@ -0,0 +1,10 @@
+export type ReposType = {
+  name: string
+  html_url: string
+  description: string
+  forks: number
+  open_issues: number
+  homepage: string
+  topics: string[]
+  stargazers_count: number
+}
---
+++ src/types/UserType/index.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+  avatar_url: string
+  name: string
+  login: string
+  location: string
+  followers: number
+  following: number
+  public_repos: number
+  html_url: string
+}
