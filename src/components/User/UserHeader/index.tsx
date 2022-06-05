a0fdb2d3e80039a74a60e035e12d594709c86195
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:09:31 -0400

    Create useHeader component

--- src/components/User/UserHeader/index.tsx
+++ src/components/User/UserHeader/index.tsx
@@ -1,17 +1,43 @@
-import { GitContext } from 'context/GitProvider'
-import { useContext } from 'react'
+import { Creadential, InfoUser, Profile } from './style'
+import { MapPinLine, UsersThree, BookBookmark, User } from 'phosphor-react'
+import { UserType } from 'types/UserType'
 
-export const UserHeader = () => {
-  const { user } = useContext(GitContext)
+interface UserHeraderProps {
+  user: UserType
+}
+export const UserHeader = ({ user }: UserHeraderProps) => {
   return (
-    <>
-      <img src={user?.avatar_url} alt={user?.login} />
-      <p>{user?.name}</p>
-      <p>{user?.login}</p>
-      <p>{user?.location}</p>
-      <p>Followers: {user?.followers}</p>
-      <p>Following: {user?.following}</p>
-      <p>Repositórios: {user?.public_repos}</p>
-    </>
+    <Profile>
+      <img src={user?.avatar_url} alt={user?.login} data-testid="img" />
+
+      <Creadential>
+        <h2>{user?.name}</h2>
+        <a href={user?.html_url} target="_blanck">
+          {user?.login}
+        </a>
+      </Creadential>
+
+      <InfoUser>
+        <span role="infoUser" title="Seguidores">
+          <UsersThree size={20} />
+          {user?.followers}
+        </span>
+
+        <span role="infoUser" title="Seguindo">
+          <User size={20} />
+          {user?.following}
+        </span>
+
+        <span role="infoUser" title="Repositórios">
+          <BookBookmark size={20} />
+          {user?.public_repos}
+        </span>
+
+        <span role="infoUser" title="Localidade">
+          <MapPinLine size={20} />
+          {user?.location}
+        </span>
+      </InfoUser>
+    </Profile>
   )
 }
