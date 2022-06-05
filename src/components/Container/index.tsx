3840d6d3f2b2820c752729ca6390285246e40c93
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:13:36 -0400

    Create logic for Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,47 @@
+import { Main, Section } from './style'
 import { Header } from '../Header'
-import { Main } from './style'
+import { useCheckStatusApi } from 'hooks/useCheckStatusApi'
+import { useState, useContext } from 'react'
+import { User } from 'components/User'
+import { GitContext } from 'context/GitProvider'
+
+import imgBg from 'assets/img/gitHub-bg.svg'
 
 export const Container = () => {
+  const [initial, setInitial] = useState<boolean>(true)
+  const [status, setStatus] = useState<boolean>(false)
+  const { user, repos, starreds, getApi } = useContext(GitContext)
+
+  const getData = async (value: string) => {
+    if (value === '') {
+      setInitial(true)
+      return
+    }
+
+    const { isExist } = await useCheckStatusApi(value)
+
+    setStatus(isExist)
+    setInitial(false)
+    getApi(value)
+  }
+
   return (
     <Main>
-      <Header />
+      <Header onclick={getData} />
+
+      <Section image={imgBg}>
+        {initial ? (
+          <></>
+        ) : (
+          <>
+            {status ? (
+              <User user={user!} repos={repos!} starreds={starreds!} />
+            ) : (
+              <p>Não existe</p>
+            )}
+          </>
+        )}
+      </Section>
     </Main>
   )
 }
