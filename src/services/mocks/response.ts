a5670f972ffe8de2a9a00face7199c443d9b7641
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:05:20 -0400

    Create mock for response

---
+++ src/services/mocks/response.ts
@@ -0,0 +1,36 @@
+export const mockUser = {
+  avatar_url: 'https://asdlfjaçldjalj',
+  name: 'Something',
+  login: 'SomethingUser',
+  location: 'Brazil',
+  followers: 9,
+  following: 5,
+  public_repos: 15,
+  html_url: 'https://ljaçljdfalj'
+}
+
+export const mockRepos = [
+  {
+    name: 'projectName',
+    html_url: 'https://dkljfalkjsf',
+    description: 'description of project',
+    forks: 4,
+    open_issues: 2,
+    homepage: 'https://flsdkjflksdjfkl',
+    topics: ['html', 'React', 'Sass'],
+    stargazers_count: 5
+  }
+]
+
+export const mockStars = [
+  {
+    name: 'projectName',
+    html_url: 'https://dkljfalkjsf',
+    description: 'description of project',
+    forks: 4,
+    open_issues: 2,
+    homepage: 'https://flsdkjflksdjfkl',
+    topics: ['html', 'React', 'Sass'],
+    stargazers_count: 5
+  }
+]
