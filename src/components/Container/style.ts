fee546eaa37da377e293bdda6f3c7d82e2958b09
Renan Souza <renantrsouza@gmail.com>
2022-06-05 02:11:41 -0400

    Add img into background

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components'
 
 export const Main = styled.main``
+
+type StyleProp = {
+  image: string
+}
+export const Section = styled.section<StyleProp>`
+  height: calc(100vh - 10rem);
+  background-image: url(${(props) => props.image});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 30rem;
+`
