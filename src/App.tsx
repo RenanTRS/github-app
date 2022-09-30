8dc4fa24ffe67b380ddd2c459cecba4666fc5d75
Renan Souza <renantrsouza@gmail.com>
2022-09-30 15:14:13 -0400

    update: implement Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Container } from 'components/Container'
 import { Header } from 'components/Header'
+import { Search } from 'components/Search'
 import { useGetTheme } from 'hooks/useGetTheme'
+import { useGetUser } from 'hooks/useGetUser'
 
 function App() {
   const theme = useGetTheme()
+  const user = useGetUser()
 
   return (
-    <Container theme={theme}>
-      <Header theme={theme} />
-    </Container>
+    <>
+      <Header theme={theme} user={user} />
+
+      <Container theme={theme}>
+        <Search theme={theme} />
+      </Container>
+    </>
   )
 }
 
