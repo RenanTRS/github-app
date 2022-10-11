dbdf4c1ad766c04eb27f29b045f99e5734f19d8b
Renan Souza <renantrsouza@gmail.com>
2022-10-11 14:40:10 -0400

    update: use data-theme in User

--- src/components/Main/User/index.tsx
+++ src/components/Main/User/index.tsx
@@ -1,23 +1,50 @@
 import { UserProps } from './types'
+import style from './User.module.scss'
+import { useGetTheme } from '../../../hooks/useGetTheme'
+import {
+  UsersThree,
+  User as UserP,
+  BookBookmark,
+  MapPinLine
+} from 'phosphor-react'
 
 export const User = ({ dataUser }: UserProps) => {
   //console.log(dataUser)
+  const theme = useGetTheme()
   return (
-    <section className="user">
-      <div className="user__data">
-        <img src={dataUser?.avatar} alt="user" />
-        <span>{dataUser?.name}</span>
-        <a href={dataUser?.url} target="_blank">
+    <section className={style.user} data-theme={theme}>
+      <div className={style.user__data}>
+        <img
+          src={dataUser?.avatar}
+          alt="user"
+          title={dataUser?.login}
+          className={style.user__data_img}
+        />
+        <span className={style.user__data_name}>{dataUser?.name}</span>
+        <a
+          href={dataUser?.url}
+          target="_blank"
+          className={style.user__data_user}
+          data-theme={theme}
+        >
           {dataUser?.login}
         </a>
       </div>
       <div className="user__media">
-        <span>followers {dataUser?.followers}</span>
+        <span>
+          <UsersThree /> {dataUser?.followers}
+        </span>
 
-        <span>following {dataUser?.following}</span>
+        <span>
+          <UserP /> {dataUser?.following}
+        </span>
 
-        <span>repositories {dataUser?.repositories}</span>
-        <span>location {dataUser?.location}</span>
+        <span>
+          <BookBookmark /> {dataUser?.repositories}
+        </span>
+        <span>
+          <MapPinLine /> {dataUser?.location}
+        </span>
       </div>
     </section>
   )
