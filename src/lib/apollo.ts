be4ea3861af09e89e4cf0f595e69fd1c27fd3805
Renan Souza <renantrsouza@gmail.com>
2022-10-11 13:23:40 -0400

    fix: add new token

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `Bearer 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`
+    Authorization: `Bearer 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`
   },
   cache: new InMemoryCache()
 })
