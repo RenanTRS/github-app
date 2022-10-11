d902ea8982fb340cb4a33996601e8c4aee672d78
Renan Souza <renantrsouza@gmail.com>
2022-10-11 12:26:49 -0400

    add: apollo provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { Container } from 'components/Container'
-import { Header } from 'components/Header'
-import { Search } from 'components/Search'
 import { useGetTheme } from 'hooks/useGetTheme'
 import { useGetUser } from 'hooks/useGetUser'
+
+import { ApolloProvider } from '@apollo/client'
+import { client } from 'lib/apollo'
+
+import { Header } from 'components/Header'
+import { Container } from 'components/Container'
+import { Search } from 'components/Search'
+import { Main } from 'components/Main'
 
 function App() {
   const theme = useGetTheme()
   const user = useGetUser()
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header theme={theme} user={user} />
 
       <Container theme={theme}>
         <Search theme={theme} />
+        {user !== null && <Main user={user} />}
       </Container>
-    </>
+    </ApolloProvider>
   )
 }
 
