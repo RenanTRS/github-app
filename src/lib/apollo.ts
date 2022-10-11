0c32b3dc3d3decd895d62c672eb6c9784937fce1
Renan Souza <renantrsouza@gmail.com>
2022-10-11 13:43:57 -0400

    add: enviroment variable

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `Bearer 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`
+    Authorization: `Bearer ${import.meta.env.VITE_KEY}`
   },
   cache: new InMemoryCache()
 })
