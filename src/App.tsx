c5f80b9a42c47e3877c52e8b254543df36ce9649
Renan Souza <renantrsouza@gmail.com>
2022-10-14 13:39:45 -0400

    refact: remove unecessaries importes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import { useGetTheme } from 'hooks/useGetTheme'
-import { useGetUser } from 'hooks/useGetUser'
 
 import { ApolloProvider } from '@apollo/client'
 import { client } from 'lib/apollo'
 
 import { Container } from 'components/Container'
-import { Route, Routes } from 'react-router-dom'
-import { Search } from 'components/Search'
-import { Main } from 'components/Main'
+import { AnimateRoutes } from 'components/AnimateRoutes'
 
 function App() {
   const theme = useGetTheme()
-  const user = useGetUser()
 
   return (
     <ApolloProvider client={client}>
       <Container theme={theme}>
-        <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="/profile" element={<Main user={user} />} />
-          <Route path="*" element={<Search />} />
-        </Routes>
+        <AnimateRoutes />
       </Container>
     </ApolloProvider>
   )
