f72b01cc566351b95dbeacfb118e569d62ad1a1d
Renan Souza <renantrsouza@gmail.com>
2022-10-17 12:47:34 -0400

    feat: create function that check if disable is true or no

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from 'react'
 import style from './Button.module.scss'
 import { ButtonProps } from './types'
 
-export const Button = ({ theme, click, styled }: ButtonProps) => {
+export const Button = ({ theme, styled, value }: ButtonProps) => {
+  const [valueInput, setValueInput] = useState('')
+
+  useEffect(() => {
+    setValueInput(value.trim())
+  }, [value])
+
   return (
     <button
       type="submit"
       className={`${style.button} ${styled}`}
-      onClick={click}
       data-theme={theme}
+      disabled={valueInput.length === 0}
     >
       Buscar
     </button>
