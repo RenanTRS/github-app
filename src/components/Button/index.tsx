dcbf1c7a797c445fb144553194ef3a3ab453fcfd
Renan Souza <renantrsouza@gmail.com>
2022-10-18 10:59:51 -0400

    remove: states and add new conditional to disable

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,18 +3,12 @@ import style from './Button.module.scss'
 import { ButtonProps } from './types'
 
 export const Button = ({ theme, styled, value }: ButtonProps) => {
-  const [valueInput, setValueInput] = useState('')
-
-  useEffect(() => {
-    setValueInput(value.trim())
-  }, [value])
-
   return (
     <button
       type="submit"
       className={`${style.button} ${styled}`}
       data-theme={theme}
-      disabled={valueInput.length === 0}
+      disabled={value === ''}
     >
       Buscar
     </button>
