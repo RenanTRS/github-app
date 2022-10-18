ae43a8b640f3e44a99f1e05ea6b0b028ff9856a2
Renan Souza <renantrsouza@gmail.com>
2022-10-18 11:16:52 -0400

    remove: unnecessaries imports

--- src/components/AnimateRoutes/index.tsx
+++ src/components/AnimateRoutes/index.tsx
@@ -1,18 +1,16 @@
-import { Main } from 'components/Main'
-import { Search } from 'components/Search'
 import { AnimatePresence } from 'framer-motion'
-import { useGetUser } from 'hooks/useGetUser'
 import { Home } from 'pages/Home'
+import { Profile } from 'pages/Profile'
 import { Route, Routes, useLocation } from 'react-router-dom'
 
 export const AnimateRoutes = () => {
-  const user = useGetUser()
   const location = useLocation()
+
   return (
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Main user={user} />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </AnimatePresence>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import { Main } from 'components/Main'
 import { useGetUser } from 'hooks/useGetUser'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+
 export const Profile = () => {
   const navigate = useNavigate()
   const user = useGetUser()
