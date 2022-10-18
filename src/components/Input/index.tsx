520b5e7eaa74e3738fd50dcb20dc50f60df070e3
Renan Souza <renantrsouza@gmail.com>
2022-10-18 10:59:08 -0400

    update: add cleaner to input value

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,13 +2,17 @@ import style from './Input.module.scss'
 import { InputProps } from './types'
 
 export const Input = ({ theme, value, change }: InputProps) => {
+  const handlerChange = (value: string) => {
+    change(value.trim())
+  }
+
   return (
     <input
       type="text"
       className={style.input}
       placeholder="Pesquisar usuário..."
       value={value || ''}
-      onChange={(e) => change(e.target.value)}
+      onChange={(e) => handlerChange(e.target.value)}
       data-theme={theme}
     />
   )
