07b2928d4b53be83081985da2ff3db992ce4f364
Renan Souza <renantrsouza@gmail.com>
2022-10-20 21:14:23 -0400

    style: refact: add new style to error component

--- src/components/ErrorG/index.tsx
+++ src/components/ErrorG/index.tsx
@@ -3,32 +3,46 @@ import { Warning } from 'phosphor-react'
 
 import style from './ErrorG.module.scss'
 import { motion } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
+import { useGetTheme } from 'hooks/useGetTheme'
+import { useState } from 'react'
 
 interface ErrorGProps {
   error: ApolloError
 }
 
 export const ErrorG = ({ error }: ErrorGProps) => {
+  const theme = useGetTheme()
+
   return (
     <motion.div
-      className={style.errorg}
+      className={style.error}
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
+        transition: {
+          delay: 0.5,
+          duration: 0.5
+        }
+      }}
+      exit={{
+        opacity: 0,
         transition: {
           duration: 0.5
         }
       }}
     >
-      {error.networkError ? (
-        <span>
-          <Warning weight="bold" /> Erro de conexão
-        </span>
-      ) : (
-        <span>
-          <Warning weight="bold" /> Usuário não encontrado
-        </span>
-      )}
+      <div className={style.error__container} data-theme={theme}>
+        <Warning weight="bold" />
+
+        {error.networkError ? (
+          <span>Erro de conexão</span>
+        ) : (
+          <>
+            <span>Usuário não encontrado</span>
+          </>
+        )}
+      </div>
     </motion.div>
   )
 }
